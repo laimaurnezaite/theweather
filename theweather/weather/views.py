5334894630dce9c3f6ba4c8fa6c2e5737084f957
Laima Urnezaite <laima.urnezaite@gmail.com>
2020-05-18 11:59:39 +0300

    create home view

--- theweather/weather/views.py
+++ theweather/weather/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# def home(request):
+#     return HttpResponse("Project 3: TODO")
+
+
+def home(request):
+    return render(request, 'index.html')
