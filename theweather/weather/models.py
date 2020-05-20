8fa606ebda276e307c5317b68d96e75d0c51d475
Laima Urnezaite <laima.urnezaite@gmail.com>
2020-05-20 14:52:33 +0300

    modify City model

--- theweather/weather/models.py
+++ theweather/weather/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
 class City(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=255)
+    geoname_id = models.IntegerField()
+    state = models.CharField(max_length=255, default="")
+    country = models.CharField(max_length=255)
+    lon = models.FloatField()
+    lat = models.FloatField()
+
 
     def __str__(self):
         return self.name
