7b675eea9cd2ffac7356fcfcb795273ef990749a
Laima Urnezaite <laima.urnezaite@gmail.com>
2020-06-01 15:56:33 +0300

    Heroku config files and updates

--- theweather/theweather/settings.py
+++ theweather/theweather/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1o5z!!0lo@a4rghlx-gkqj=k1k)k4*l(ds!z0m7t=$@+ub-1#^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
